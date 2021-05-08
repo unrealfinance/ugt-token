122e07e9f42c9e9aa6e2b102daed1dab0b086c09
aniket965 <aniket965.as@gmail.com>
2021-05-08 18:42:21 +0530

    feat: adds deployment script

---
+++ deploy/00_deploy_contracts.ts
@@ -0,0 +1,26 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import {ethers} from 'hardhat';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deterministic } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const totalSupply = ethers.utils.parseEther("5000000000");
+  const { address: addressUnrealToken, deploy: deployUnrealToken } = await deterministic("UnrealToken", {
+    from: deployer,
+    args: [totalSupply],
+    log: true,
+  });
+
+  await deployUnrealToken();
+
+  const { deploy: deployTokenVesting } = await deterministic("TokenVesting", {
+    from: deployer,
+    args: [addressUnrealToken],
+    log: true,
+  });
+
+  await deployTokenVesting();
+};
+export default func;
